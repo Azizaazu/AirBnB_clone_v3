300580f9a5e4e8f8d11310dbc643e5127e92099b
Azizaazu <aynaou.aziza98@gmail.com>
2024-01-08 14:35:52 -0800

    update

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Importing everything from index module
+from api.v1.views.index import *
